08ecf6f79a50dd0988931f1bccb3165f4c418bfa
Nilton J F <>
2018-03-21 11:16:03 -0300

    inicio exemplo retorno parametro

---
+++ em_sala/PesquisaApp/app/src/main/java/devmob2018/com/pesquisaapp/DadosPessoaisActivity.java
@@ -0,0 +1,15 @@
+package devmob2018.com.pesquisaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DadosPessoaisActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_pessoais);
+    }
+
+
+}
---
+++ em_sala/PesquisaApp/app/src/main/java/devmob2018/com/pesquisaapp/PrincipalActivity.java
@@ -0,0 +1,18 @@
+package devmob2018.com.pesquisaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrincipalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+    public void chamar(View v){
+
+    }
+}
